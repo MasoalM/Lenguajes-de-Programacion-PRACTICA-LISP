160bbb6789da40dbdfd0ad8542ed81ad7cb99efa
Marcos Socías <105206059+MasoalM@users.noreply.github.com>
2025-04-11 19:10:28 +0200

    V 0.6

--- creacioLaberints.lisp
+++ creacioLaberints.lisp
@@ -1,8 +1,32 @@
-(setq lWidth 25)
-(setq lHeight 25)
+(setq lWidth 10)
+(setq lHeight 10)
+(setq entradaX (+ (random lWidth) 1))
+(setq entradaY (+ (random lHeight) 1))
+(setq actualX entradaX)
+(setq actualY entradaY)
 
-(defun genera (nom-fitxer)
+(defun genera ()
+    ; abrir fichero en modo escribir
+    ; let de nombre f al fichero
+    ; (escribir f (crea-matriu ...))
+    (crea-matriu lWidth lHeight)
 
 )
 
-(genera laberinto)
+(defun crea-matriu (x y)
+    (cond
+        ((= 0 x) '())
+        (t (append (crea-matriu (- x 1) y) (list (crea-fila x y))))
+    )
+)
+
+(defun crea-fila (x y)
+    (cond
+        ((= 0 y) '())
+        ((and (= entradaX x) (= entradaY y)) (cons 'entrada (crea-fila x (- y 1))))
+        (t (cons 'paret (crea-fila x (- y 1))))
+    )
+    
+)
+
+(print (genera))
