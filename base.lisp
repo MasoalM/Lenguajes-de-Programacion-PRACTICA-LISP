9716642a9835f2233ccbd0acf0f609683ef77a87
Marcos Socías <105206059+MasoalM@users.noreply.github.com>
2025-04-17 12:47:11 +0200

    V 0.8

--- base.lisp
+++ base.lisp
@@ -2,8 +2,8 @@
 ; Autores : Marcos Socías Alberto y Hugo Valls Sabater
 ; Fecha última modificación: 04/04/2025
 
-(setq xi 10) ; posicion x donde empieza a pintar
-(setq yi 10) ; posicion y donde empieza a pintar 
+(setq xi 20) ; posicion x donde empieza a pintar
+(setq yi 0) ; posicion y donde empieza a pintar 
 (setq m 14)  ; tamaño de un cuadrado
 (setq px 10) ; posicion inicial x en la matriz del jugador   
 (setq py 10) ; posicion inicial y en la matriz del jugador
@@ -34,16 +34,16 @@
               '(paret paret paret paret paret paret paret paret paret paret paret paret paret cami cami paret paret paret paret paret paret paret paret paret paret)
               '(paret paret paret paret paret paret paret paret paret paret paret paret paret cami paret paret paret paret paret paret paret paret paret paret paret)
               '(paret paret paret paret paret paret paret cami cami cami paret paret paret cami paret paret paret paret paret paret paret paret paret paret paret)  ; HACERLO ÓPTIMO
-              '(paret paret paret paret paret paret paret paret paret cami paret paret paret cami paret paret paret paret paret paret paret paret paret paret paret)
-              '(paret paret paret paret paret paret paret paret paret cami cami cami cami cami paret paret paret paret paret paret paret paret paret paret paret)
-              '(paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
-              '(paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
-              '(paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
-              '(paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
-              '(paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
-              '(paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
-              '(paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
-              '(paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
+              '(paret paret paret paret paret paret paret cami paret cami cami paret paret cami paret paret paret paret paret paret paret paret paret paret paret)
+              '(paret paret paret paret paret paret paret cami paret paret cami cami cami cami paret paret paret paret paret paret paret paret paret paret paret)
+              '(paret paret paret paret paret paret paret cami paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
+              '(paret paret paret paret paret paret cami cami paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
+              '(paret paret paret paret paret cami cami paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
+              '(paret paret paret paret paret cami paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
+              '(paret paret paret paret cami cami paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
+              '(paret paret paret paret cami paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
+              '(paret cami cami cami cami paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
+              '(paret cami paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)
               '(paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret paret)))
 
 (defun quadrat (x m)
@@ -59,16 +59,16 @@
     (cond
         ((= n 0) nil)
         (t
-            (apply 'color (get 'colors (car l)))    ; TOO FEW ARGUMENTS: Falla cuando hago (apply 'color (get 'colors (car l)))
+            (apply 'color (get 'colors (car l)))
             (moverel m 0)
             (quadrat m m)
             ; pintar (o no) al jugador
             (cond
                 ((and (= px 0) (= py 0)) 
                     (apply 'color (get 'colors 'verde))
-                    (moverel 2 2)
+                    (moverel 2 3)
                     (quadrat (- m 5) (- m 5))
-                    (moverel -2 -2)
+                    (moverel -2 -3)
                 )
             )
             (fila-quadrats (- n 1) (cdr l) (- px 1) py)
@@ -82,13 +82,25 @@
         (t 
             (fila-quadrats lWidth (car l) px py)
             ; siguiente fila
-            (moverel (- (* m lWidth)) m)
+            (moverel (- (* m lWidth)) (- m))
             (pinta (cdr l) px (- py 1))
         )
     )
 )
 
-; CREAR FUNCIÓN "obtenir-posicio" QUE OBTIENE EL COLOR DE UNA POSICIÓN A PARTIR DE PX y PY
+(defun obtenir-posicio (l x y)
+    (cond
+        ((= y 0) (obtenir-posicio-exacta (car l) x))
+        (t (obtenir-posicio (cdr l) x (- y 1)))
+    )
+)
+
+(defun obtenir-posicio-exacta (l x)
+    (cond
+        ((= x 0) (car l))
+        (t (obtenir-posicio-exacta (cdr l) (- x 1)))
+    )
+)
 
 (defun dreta (px)
     (mod (+ px 1) lWidth)
@@ -114,18 +126,18 @@
 
 (defun passa (l px py)
     (cls)
-    (move xi yi)
+    (move xi (+ yi (* m lHeight)))
     (pinta l px py)
     (setq tecla (get-key))
     (cond
         ; si la tecla pulsada es A o flecha izquierda
-        ((or (= 65 tecla) (= 97 tecla) (= 331 tecla)) (passa l (esquerra px) py))
+        ((and (or (= 65 tecla) (= 97 tecla) (= 331 tecla)) (not (equal (obtenir-posicio l (esquerra px) py) 'paret))) (passa l (esquerra px) py))
         ; si la tecla pulsada es D o flecha derecha
-        ((or (= 68 tecla) (= 100 tecla) (= 333 tecla)) (passa l (dreta px) py))
-        ; si la tecla pulsada es W o flecha arriba
-        ((or (= 87 tecla) (= 119 tecla) (= 328 tecla)) (passa l px (amunt py)))
-        ; si la tecla pulsada es S o flecha abajo
-        ((or (= 83 tecla) (= 115 tecla) (= 336 tecla)) (passa l px (abaix py)))
+        ((and (or (= 68 tecla) (= 100 tecla) (= 333 tecla)) (not (equal (obtenir-posicio l (dreta px) py) 'paret))) (passa l (dreta px) py))
+        ; si la tecla pulsada es W o flecha arriba (irá abajo porque se pinta al revés)
+        ((and (or (= 87 tecla) (= 119 tecla) (= 328 tecla)) (not (equal (obtenir-posicio l px (abaix py)) 'paret))) (passa l px (abaix py)))
+        ; si la tecla pulsada es S o flecha abajo (irá arriba porque se pinta al revés)
+        ((and (or (= 83 tecla) (= 115 tecla) (= 336 tecla)) (not (equal (obtenir-posicio l px (amunt py)) 'paret))) (passa l px (amunt py)))
         ; si la tecla pulsada es ESC
         ((= 27 tecla) l)
         ; si no, se llama recursivamente tal cual
@@ -133,4 +145,4 @@
     )
 )
 
-(passa l px py)
+(passa l px (- lHeight (+ py 1)))
--- creacioLaberints.lisp
+++ creacioLaberints.lisp
@@ -16,7 +16,38 @@
 ; algoritmo DFS
 (defun crea-cami (l x y)
     ; moverme en posición random y modificar x e y según donde vaya avanzando
-    (crea-cami l (+ x 1) y)
+    (let z (random 4))
+    (cond
+        ((and (= z 0) (= (contador-camins-veins l (+ x 1) y) 1)) (crea-cami l (+ x 1) y)) ; and es pared and no se junta con camino (excepto el actual)
+        ((and (= z 1) (= (contador-camins-veins l (- x 1) y) 1)) (crea-cami l (- x 1) y))
+        ((and (= z 2) (= (contador-camins-veins l x (+ y 1)) 1)) (crea-cami l x (+ y 1)))
+        ((and (= z 3) (= (contador-camins-veins l x (- y 1)) 1)) (crea-cami l x (+ y 1)))
+        ; cómo hago para que si falla uno, pruebe otros antes del caso final?
+        (t )
+    )
+)
+
+(defun contador-camins-veins (l x y)
+    (let c 0)
+    (cond ((not (eq (obtenir-posicio l (+ x 1) y) 'paret)) (+ c 1)))  ; eq, = o equal¿?
+    (cond ((not (eq (obtenir-posicio l (- x 1) y) 'paret)) (+ c 1)))
+    (cond ((not (eq (obtenir-posicio l x (+ y 1)) 'paret)) (+ c 1)))
+    (cond ((not (eq (obtenir-posicio l x (- y 1)) 'paret)) (+ c 1)))
+    ; cómo hago que devuelva c?
+)
+
+(defun obtenir-posicio (l x y)
+    (cond
+        ((= y 0) (obtenir-posicio-exacta (car l) x))
+        (t (obtenir-posicio (cdr l) x (- y 1)))
+    )
+)
+
+(defun obtenir-posicio-exacta (l x)
+    (cond
+        ((= x 0) (car l))
+        (t (obtenir-posicio-exacta (cdr l) (- x 1)))
+    )
 )
 
 (defun crea-matriu (x y)
@@ -31,8 +62,7 @@
         ((= 0 y) '())
         ((and (= entradaX x) (= entradaY y)) (cons 'entrada (crea-fila x (- y 1))))
         (t (cons 'paret (crea-fila x (- y 1))))
-    )
-    
+    )  
 )
 
 (print (genera))
